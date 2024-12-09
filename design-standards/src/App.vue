fe4f589b420c09f5c56369af10d6a629c580d318
marwan.maher <marwan.maher@id3m.com>
2024-12-09 15:49:41 +0200

    asd

--- design-standards/src/App.vue
+++ design-standards/src/App.vue
@@ -12,30 +12,11 @@
 
   <footer class="fixed bottom-0 left-0 w-full p-2 shadow bg-gray-800">
     <div
-      class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between"
+      class="w-full flex justify-center mx-auto text-center max-w-screen-xl p-4 md:flex md:items-center md:justify-between"
     >
-      <span class="text-sm sm:text-center text-gray-400"
-        >© 2024 Smart Solutions Ltd. All Rights Reserved.
-      </span>
-      <ul
-        class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-400 sm:mt-0"
-      >
-        <!-- <li>
-              <p  class="hover:underline me-4 md:me-6">ENG/ Omar Mohamed Atef</p>
-          </li>
-        <li>
-              <a  class="hover:underline me-4 md:me-6">Prof/ Khaled Mohamed Dewidar</a>
-          </li>
-          <li>
-              <p  class="hover:underline me-4 md:me-6">Prof/ Gehan Ahmed Nagy</p>
-          </li>  -->
-
-        <li>
-          <p class="hover:underline me-4 md:me-6">
-            Provided by: O.M.A., K.M.D., and G.A.N
-          </p>
-        </li>
-      </ul>
+      <p class="hover:underline me-4 md:me-6 text-gray-400">
+        Provided by: O.M.A.
+      </p>
     </div>
   </footer>
 </template>
